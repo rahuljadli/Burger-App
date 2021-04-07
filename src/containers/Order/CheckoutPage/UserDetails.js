0fdc44173566aa00b730c2be08e63922f3dad345
rahul jadli <rahuljadli19@gmail.com>
2021-04-07 18:41:42 +0530

    made some minute changes so that the data from the form can be retrieved and uploaded to the server in the userDetails component

--- src/containers/Order/CheckoutPage/UserDetails.js
+++ src/containers/Order/CheckoutPage/UserDetails.js
@@ -5,13 +5,23 @@ import axios from '../../../AxiosInstance';
 class UserDetails extends Component{
 
 state={
-    users:{
-        name:'rahul jadli',
+    
+        name:'rahul',
         age:'23',
         email:'rahul@gmail.com'
-    }
+   
 }
     orderHandler(){
+
+
+        // Code for retreiving data from form
+        const users={
+            name:this.state.name,
+            age:this.state.age,
+            email:this.state.email
+        }
+        console.log("Showing user details")
+
         console.log("order Placed")
         const ingre=this.props.ingredients
         const updatedIngredient={
@@ -24,39 +34,74 @@ state={
         const order={
             ingredients:updatedIngredient,
             price:this.props.totalCost,
-            customer:this.state.users
+            customer:users
         }
         axios.post('orders.json',order).then(
             response=>{
-                console.log(response);
+                console.log("placed")
             }
         ).catch(error=>{
             console.log(error);
         })
-        
 
-
+        this.props.history.push('/')
     }
+    handleName=(event)=>{
+        
+        this.setState({
+            name:event.target.value
+        });
+    }
+    handleAge=(event)=>{
+        this.setState({
+            age:event.target.value
+        });
+    }
+    handleEmail=(event)=>{
+        this.setState({
+            email:event.target.value
+        });
+    }
+    
     render(){
-        console.log(this.props)
         return(
             <div className={classes.UserDetails}>
                 <h4>Enter Your Details</h4>
-                <form style={{textAlign:'center',
+                <form
+                onSubmit={()=>this.orderHandler(this)}
+                
+                style={{textAlign:'center',
                                paddingLeft:'370px' }}>
-                <input  className={classes.Input} name="name"
+                <input required 
+                value={this.state.name} 
+                onChange={this.handleName.bind(this)}
+
+                className={classes.Input} name="name"
                  type="text" placeholder="Enter your Name" />
-                <input className={classes.Input} name="age"
-                type="text" placeholder="Enter your age" />
-                <input className={classes.Input} type="text" name="email"
+
+                <input required 
+                className={classes.Input} 
+                value={this.state.age} 
+                onChange={this.handleAge.bind(this)}
+                name="age"
+                type="text" 
+                placeholder="Enter your age" />
+                <input required 
+                value={this.state.email} 
+                onChange={this.handleEmail.bind(this)}
+                className={classes.Input}
+                 type="email" 
+                 name="email"
                  placeholder="Enter your Email ID" />
-               
-                    </form>
-                    <Button 
+                          <Button
+                         
         buttonType="Success" 
-            clicked={()=>this.orderHandler()}>
+            >
             Confirm
         </Button>
+                    </form>
+          
+                
             </div>
                     )
     }
