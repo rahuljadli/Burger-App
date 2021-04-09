d3a3030cc03d7ba0212e35a5c7c5ccfb270c3bdd
rahul jadli <rahuljadli19@gmail.com>
2021-04-09 14:15:46 +0530

    Added redux and react-redux package for handling states through redux for that created reducer and action file to store constant, also created store and wraped the index.js file around it

---
+++ src/Store/reducer.js
@@ -0,0 +1,17 @@
+import * as actionTypes from './action';
+const initialState=
+{
+    ingredients:{
+    cheese:0,
+   salad:0,
+    bacon:0,
+    meat:0
+                },
+totalCost:0
+}
+
+const reducer=(state=initialState,action)=>{
+
+}
+
+export default reducer;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './Store/reducer';
+
+const store=createStore(reducer)
+
 ReactDOM.render(
   <React.StrictMode>
+  <Provider store={store}>
   <BrowserRouter><App /></BrowserRouter>
-    
+  </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
