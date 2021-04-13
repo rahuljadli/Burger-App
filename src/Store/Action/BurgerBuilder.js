3624cbedbfa348b09762af94210eba565e04d40a
rahul jadli <rahuljadli19@gmail.com>
2021-04-13 17:39:38 +0530

    Modified the store a little Made BurgerBuilder in action to handle action so we can run ascynchronous code and in reducer changed named from reducer so it will handle only burgerBuildingReducer, used thunk middleware to wire redux to it so we can create actionCreators for basically running asynchronous code al

---
+++ src/Store/Action/BurgerBuilder.js
@@ -0,0 +1,19 @@
+
+// This  Action is responsible for all the action that are dispatched w.r.t Burger Building
+import * as actionTypes from '../Action/action'
+
+export const addIngredient=(name)=>{
+    return{
+        type: actionTypes.ADD_INGREDIENT,
+          ingredientName: name
+
+    }
+}
+
+
+export const removeIngredient=(name)=>{
+    return{
+        type: actionTypes.DELETE_INGREDIENT,
+        ingredientName: name
+    }
+}
---
+++ src/Store/Action/action.js
@@ -0,0 +1,5 @@
+
+// This actionType is used to store constants
+export const ADD_INGREDIENT='ADD_INGREDIENT';
+export const DELETE_INGREDIENT='DELETE_INGREDIENT';
+export const RESET_INGREDIENT='RESET_INGREDIENT';
